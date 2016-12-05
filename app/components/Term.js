e75a35f1ae1e2ec94c9020251d941e64dab3566c
Jakob Gillich <jakob@gillich.me>
2016-12-05 01:00:13 +0100

    accept term input after ws connection is open

--- app/components/Term.js
+++ app/components/Term.js
@@ -42,7 +42,9 @@ export default class Term extends Component {
         }
       }
 
-      term.runCommandClass(HTerm)
+      ws.onopen = () => {
+        term.runCommandClass(HTerm)
+      }
     })
   }
 
