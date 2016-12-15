3250a0b5b22a60563b0da352c2c0ac59cd7cf236
Jakob Gillich <jakob@gillich.me>
2016-12-15 20:38:53 +0100

    remove faq

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,11 +1,9 @@
-import faq from './faq.md!'
 import notfound from './notfound.md!'
 import tos from './tos.md!'
 import boxes from './boxes.md!'
 import terminal from './terminal.md!'
 
 export default {
-  faq,
   notfound,
   tos,
   boxes,
