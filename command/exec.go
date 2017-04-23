dd744c641e6c4107fc9b9e7f187cbe9332fecc7e
Jakob Gillich <jakob@gillich.me>
2017-04-23 21:04:29 +0200

    exec support

--- command/exec.go
+++ command/exec.go
@@ -1,10 +1,22 @@
 package command
 
 import (
+	"os"
+
+	"gitlab.com/faststack/machinestack/client"
 	"gopkg.in/urfave/cli.v2"
 )
 
 // Exec executes a command and attaches to its tty
 func Exec(c *cli.Context) error {
-	panic("TODO")
+	name := c.Args().Get(0)
+
+	client := client.New(c.String("machinestack"), c.String("token"))
+	sessionID, err := client.SessionCreate(name)
+
+	if err != nil {
+		return err
+	}
+
+	return client.SessionIO(sessionID, os.Stdin, os.Stdout)
 }
